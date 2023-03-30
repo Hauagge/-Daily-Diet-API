39a53ac59bac3f9c3e98cfab3dddcdc916c8643a
Hauagge <gabriel.dev@agroflux.ind.br>
2023-03-30 13:04:00 -0300

    repository meals

---
+++ src/repository/Meal/MealRepository.ts
@@ -0,0 +1,50 @@
+import { prisma } from '@/database/prismaClient';
+import { IMealRepository } from './IMealRepository';
+import { Prisma, Meal } from '@prisma/client';
+
+export class MealRepository implements IMealRepository {
+    async create({
+        name,
+        description,
+        isFitness,
+        dateAndHour,
+        userId,
+    }: Prisma.MealUncheckedCreateInput): Promise<Meal> {
+        return await prisma.meal.create({
+            data: {
+                userId,
+                name,
+                description,
+                isFitness,
+                dateAndHour,
+            },
+        });
+    }
+    async findById(id: string): Promise<Meal | null> {
+        return await prisma.meal.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+
+    async update({
+        id,
+        name,
+        description,
+        isFitness,
+        dateAndHour,
+    }: Prisma.MealUncheckedUpdateInput): Promise<Meal> {
+        return await prisma.meal.update({
+            where: {
+                id: id as string,
+            },
+            data: {
+                name,
+                description,
+                isFitness,
+                dateAndHour,
+            },
+        });
+    }
+}
