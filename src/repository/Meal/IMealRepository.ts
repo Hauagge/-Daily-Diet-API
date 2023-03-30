3f600cc20cfdf8a93efcf972e1aa9b82f0734fc5
Hauagge <gabriel.dev@agroflux.ind.br>
2023-03-30 13:03:50 -0300

    repository interface

---
+++ src/repository/Meal/IMealRepository.ts
@@ -0,0 +1,21 @@
+import { Prisma, Meal } from '@prisma/client';
+
+interface IMealRepository {
+    create({
+        name,
+        description,
+        isFitness,
+        dateAndHour,
+        userId,
+    }: Prisma.MealUncheckedCreateInput): Promise<Meal>;
+    findById(id: string): Promise<Meal | null>;
+    update({
+        id,
+        name,
+        description,
+        isFitness,
+        dateAndHour,
+    }: Prisma.MealUncheckedUpdateInput): Promise<Meal>;
+}
+
+export { IMealRepository };
