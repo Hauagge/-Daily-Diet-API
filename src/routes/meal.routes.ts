0cb28f1ce99320f98f71716ccc18343884ef5c51
Hauagge <gabriel.dev@agroflux.ind.br>
2023-03-31 20:34:00 -0300

    updated meal route

--- src/routes/meal.routes.ts
+++ src/routes/meal.routes.ts
@@ -1,7 +1,9 @@
 import { verifyJwt } from '@/middlewares/check-sessions-id-exists';
 import { FastifyInstance } from 'fastify';
 import { createMealController } from '../controller/Meal/CreateMealController';
+import { updateMealController } from '../controller/Meal/UpdateMealController';
 
 export async function MealRoutes(app: FastifyInstance, options: any) {
-    app.post('/Meal', { onRequest: [verifyJwt] }, createMealController);
+    app.post('/meal', { onRequest: [verifyJwt] }, createMealController);
+    app.put('/meal', { onRequest: [verifyJwt] }, updateMealController);
 }
